f476f0b3caff5f69af3acf4bfd7b5f3ae7e667a2
Thomas Edvalson <machin3@gmail.com>
2014-02-01 00:11:57 -0500

    Made Screen's constructor into just initializer list

--- include/cpp3ds/Screen.h
+++ include/cpp3ds/Screen.h
@@ -13,7 +13,8 @@ namespace cpp3ds {
 		int frame1, frame2;
 		int width, height;
 	public:
-		Screen(int addr1, int addr2, int w, int h);
+		Screen(int addr1, int addr2, int w, int h):
+		        frame1(addr1), frame2(addr2), width(w), height(h) {}
 		int getWidth(){ return width; }
 		int getHeight(){ return height; }
 		void setPixelAddress(int addr, Color color);
--- src/Screen.cpp
+++ src/Screen.cpp
@@ -6,13 +6,6 @@
 
 namespace cpp3ds {
 
-	Screen::Screen(int addr1, int addr2, int w, int h){
-		frame1 = addr1;
-		frame2 = addr2;
-		width = w;
-		height = h;
-	}
-
 	void Screen::setPixelAddress(int addr, Color color){
 		write_byte(addr, color.b);
 		write_byte(addr+1, color.g);
@@ -27,17 +20,17 @@ namespace cpp3ds {
 	}
 
 	void Screen::clear(Color color){
-		// Just clear to black for now
-		memset((char*)frame1, 0, width*height*3);
-		memset((char*)frame2, 0, width*height*3);
-		// A little slow, maybe try memcpy?
-		// char* frameptr1 = (char*)frame1;
-		// char* frameptr2 = (char*)frame2;
-		// for (int i = 0; i < width*height; ++i){
-		// 	*frameptr1++ = *frameptr2++ = color.b;
-		// 	*frameptr1++ = *frameptr2++ = color.g;
-		// 	*frameptr1++ = *frameptr2++ = color.r;
-		// }
+		// Just clear to black
+		// memset((char*)frame1, 0, width*height*3);
+		// memset((char*)frame2, 0, width*height*3);
+		// TODO: Too slow, maybe try memcpy?
+		char* frameptr1 = (char*)frame1;
+		char* frameptr2 = (char*)frame2;
+		for (int i = 0; i < width*height; ++i){
+			*frameptr1++ = *frameptr2++ = color.b;
+			*frameptr1++ = *frameptr2++ = color.g;
+			*frameptr1++ = *frameptr2++ = color.r;
+		}
 	}
 
 	void Screen::draw(Drawable& obj, int x, int y) {
