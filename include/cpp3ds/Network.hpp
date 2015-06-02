3f38b29bd954966db33c890b649f02aa0a536f53
Thomas Edvalson <machin3@gmail.com>
2015-06-01 20:15:44 -0400

    Initial port of SFML Network module

--- include/cpp3ds/Network.hpp
+++ include/cpp3ds/Network.hpp
@@ -2,14 +2,16 @@
 #define CPP3DS_NETWORK_HPP
 
 #include <cpp3ds/System.hpp>
-//#include <cpp3ds/Network/Ftp.hpp>
-//#include <cpp3ds/Network/Http.hpp>
-//#include <cpp3ds/Network/IpAddress.hpp>
-//#include <cpp3ds/Network/Packet.hpp>
-//#include <cpp3ds/Network/SocketSelector.hpp>
-//#include <cpp3ds/Network/TcpListener.hpp>
-//#include <cpp3ds/Network/TcpSocket.hpp>
-//#include <cpp3ds/Network/UdpSocket.hpp>
+#include <cpp3ds/Network/Ftp.hpp>
+#include <cpp3ds/Network/Http.hpp>
+#include <cpp3ds/Network/IpAddress.hpp>
+#include <cpp3ds/Network/Packet.hpp>
+#include <cpp3ds/Network/Socket.hpp>
+#include <cpp3ds/Network/SocketHandle.hpp>
+#include <cpp3ds/Network/SocketSelector.hpp>
+#include <cpp3ds/Network/TcpListener.hpp>
+#include <cpp3ds/Network/TcpSocket.hpp>
+#include <cpp3ds/Network/UdpSocket.hpp>
 
 #endif
 
