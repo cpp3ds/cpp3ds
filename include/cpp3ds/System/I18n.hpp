f407f9e3644b532ce844ae9fa2a55e1339dc17b4
Cruel <machin3@gmail.com>
2015-09-16 16:33:31 -0400

    Implement basic I18n class

---
+++ include/cpp3ds/System/I18n.hpp
@@ -0,0 +1,92 @@
+#ifndef CPP3DS_I18N_HPP
+#define CPP3DS_I18N_HPP
+
+#ifndef EMULATION
+#include <3ds.h>
+#endif
+#include <iostream>
+#include <stdio.h>
+#include <map>
+#include <cstring>
+#include <memory>
+#include <cpp3ds/Config.hpp>
+
+#define _(key, ...) (cpp3ds::I18n::getInstance().translate(key, ##__VA_ARGS__))
+
+
+namespace {
+
+	template<typename ... Args>
+	std::string string_format( const std::string& format, Args ... args )
+	{
+		size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
+		std::unique_ptr<char[]> buf( new char[ size ] );
+		snprintf( buf.get(), size, format.c_str(), args ... );
+		return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
+	}
+}
+
+
+namespace cpp3ds {
+
+enum Language {
+	Japanese    = 0,
+	English     = 1,
+	French,
+	German,
+	Italian,
+	Spanish,
+	ChineseSimplified,
+	Korean,
+	Dutch,
+	Portuguese,
+	Russian,
+	ChineseTraditional,
+};
+
+class I18n {
+public:
+
+	static I18n& getInstance();
+
+	static void loadLanguage(Language language);
+
+	static inline void loadLanguageFile(const std::string& filename);
+
+	static Language getLanguage();
+
+	template<typename ... Args>
+	const std::string translate(const char* key, Args ... args) const {
+		std::string trans;
+		TranslationMap::const_iterator it = m_content.find(key);
+		if (it == m_content.end())
+			trans = std::string(key);
+		else
+			trans = it->second;
+		return string_format(trans, args ...);
+	}
+
+	template<typename ... Args>
+	const std::string translate(const std::string& key, Args ... args) const {
+		return translate(key.c_str(), args ...);
+	}
+
+	const std::string getLangString(const Language language) const;
+
+private:
+	I18n();
+
+	I18n(const I18n &);
+
+	bool loadFromFile(const std::string filename);
+
+	bool loadFromLanguage(const Language language);
+
+	typedef std::map <std::string, std::string> TranslationMap;
+	TranslationMap m_content;
+	Language m_language;
+};
+
+} // namespace cpp3ds
+
+#endif // CPP3DS_I18N_HPP
---
+++ src/cpp3ds/System/I18n.cpp
@@ -0,0 +1,143 @@
+#include <clocale>
+#include <cstring>
+#include <fstream>
+
+#include <cpp3ds/System/Err.hpp>
+#include <cpp3ds/System/I18n.hpp>
+#include <cpp3ds/System/String.hpp>
+
+#define TOKEN_COMMENT  '#'
+
+
+namespace {
+
+static bool replace(std::string& str, const std::string& from, const std::string& to) {
+	size_t start_pos = str.find(from);
+	if(start_pos == std::string::npos)
+		return false;
+	str.replace(start_pos, from.length(), to);
+	return true;
+}
+
+static void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+	if(from.empty())
+		return;
+	size_t start_pos = 0;
+	while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+	}
+}
+
+} // namespace
+
+namespace cpp3ds {
+
+
+I18n& I18n::getInstance()
+{
+	static I18n self;
+	return self;
+}
+
+
+I18n::I18n()
+{
+	Uint8 langcode;
+	// TODO: use Service class to enable CFGU here
+#ifdef EMULATION
+	langcode = 1; // TODO: get actual locale of PC
+#else
+	CFGU_GetSystemLanguage(&langcode);
+#endif
+	m_language = static_cast<Language>(langcode);
+	loadFromLanguage(m_language);
+}
+
+
+const std::string I18n::getLangString(const Language langcode) const
+{
+	switch (langcode) {
+		case Japanese:           return "jp";
+		case English:            return "en";
+		case French:             return "fr";
+		case German:             return "de";
+		case Italian:            return "it";
+		case Spanish:            return "es";
+		case ChineseSimplified:  return "zh";
+		case Korean:             return "ko";
+		case Dutch:              return "nl";
+		case Portuguese:         return "pt";
+		case Russian:            return "ru";
+		case ChineseTraditional: return "tw";
+		default:                 return "en";
+	}
+}
+
+
+void I18n::loadLanguage(Language language)
+{
+	getInstance().loadFromLanguage(language);
+}
+
+
+Language I18n::getLanguage()
+{
+	return static_cast<Language>(getInstance().m_language);
+}
+
+
+bool I18n::loadFromLanguage(const Language language)
+{
+	std::string filename = "lang/" + getLangString(language) + ".lang";
+	return loadFromFile(filename);
+}
+
+
+void I18n::loadLanguageFile(const std::string& filename)
+{
+	getInstance().loadFromFile(filename);
+}
+
+
+bool I18n::loadFromFile(const std::string filename)
+{
+	std::ifstream file(filename);
+	if (file)
+	{
+		m_content.clear();
+		std::string line;
+		std::string content;
+		const std::map<std::string, std::string> replaceList = {
+				{"\\n", "\n"}, {"\\'", "\'"}
+		};
+
+		while (std::getline(file, line)) {
+			if (line.empty() || line[0] == TOKEN_COMMENT)
+				continue;
+
+			std::string key = line;
+			while (std::getline(file, content)) {
+				if (!content.empty() && content[0] != TOKEN_COMMENT)
+					break;
+			}
+
+			replaceAll(key, "\\\\", "\\");
+			replaceAll(content, "\\\\", "\\");
+
+			for (auto& s : replaceList) {
+				replaceAll(key, s.first, s.second);
+				replaceAll(content, s.first, s.second);
+			}
+
+			m_content[key] = content;
+		}
+		return true;
+
+	} else {
+		err() << "Failed to load language file: " << filename << std::endl;
+	}
+	return false;
+}
+
+}
